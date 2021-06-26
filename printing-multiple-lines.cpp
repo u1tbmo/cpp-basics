6a72deb5ecfe4c149db2f58e20cc97ff3935d09a
Euan Jed Tabamo <tabamoeuan@gmail.com>
2021-06-26 23:03:14 +0800

    Added printing-multiple-lines, updated settings.json

---
+++ printing-multiple-lines.cpp
@@ -0,0 +1,10 @@
+//Printing multiple lines by u1tbmo
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    cout << "This is a program created by u1tbmo." << endl;
+    cout << "Goodbye!" << endl;
+    return 0;
+}
