8084fd3d1fa1b6e06b07630011e5cda86a7a9cf7
Euan Jed Tabamo <tabamoeuan@gmail.com>
2021-06-26 23:14:19 +0800

    Added variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,19 @@
+//Variables by u1tbmo
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Variables
+    string pronouns1 = "He";
+    string pronouns2 = "they";
+    string s_want = " wants ";
+    string p_want = " want ";
+    string food1 = "Ice Cream";
+    string food2 = "Cake";
+
+    //Output
+    cout << pronouns1 << s_want << food1 << "." << endl;
+    cout << "Although, " << pronouns2 << p_want << food2 << "." << endl;
+
+    return 0;
+}
